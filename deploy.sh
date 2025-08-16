01cf819c6acbbbc60fbea352c4dc4295408015c8
Nachichuri <nachichuri@gmail.com>
2025-08-16 17:40:16 -0300

    feat: add codeowners file

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ git fetch origin main
 if [ $(git rev-list HEAD...origin/main --count) != 0 ]; then
     echo "Changes detected, deploying..."
     git pull origin main
-    docker-compose down
-    docker-compose build --no-cache
-    docker-compose up -d
+    docker compose down
+    docker compose build --no-cache
+    docker compose up -d
     echo "Deployment complete"
 fi
