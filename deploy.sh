a46d11072dfa5df706d2c0e1a4275bf668b27eea
Nachichuri <nachichuri@gmail.com>
2025-08-16 17:31:24 -0300

    feat: add bash for automatic redeploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /docker/nerdearla-mcp
+
+git fetch origin main
+
+if [ $(git rev-list HEAD...origin/main --count) != 0 ]; then
+    echo "Changes detected, deploying..."
+    git pull origin main
+    docker-compose down
+    docker-compose build --no-cache
+    docker-compose up -d
+    echo "Deployment complete"
+else
+    echo "No changes"
+fi
